91a9597c7bade6c16dc02102eb16efd633020a04
Diego Viana <dtviana90@gmail.com>
2024-05-04 19:17:20 -0300

    feat: adiciona arquivo model base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RestaurantServiceSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

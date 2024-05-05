6ebaea813dbe3b5556f56287fbcc8e30d9bdc087
Diego Viana <dtviana90@gmail.com>
2024-05-05 20:33:41 -0300

    feat: adiciona swagger ao projeto

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestaurantServiceSystem", Version = "v1.0.0" });
+});
 
 var app = builder.Build();
 
@@ -19,6 +30,14 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+// Use Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestaurantServiceSystem V1.0.0");
+    c.RoutePrefix = "swagger"; // Define a rota para o Swagger UI
+});
 
 app.MapControllerRoute(
     name: "default",
--- RestaurantServiceSystem.Infrastructure/obj/Release/net6.0/RestaurantServiceSystem.Infrastructure.AssemblyInfo.cs
+++ RestaurantServiceSystem.Infrastructure/obj/Release/net6.0/RestaurantServiceSystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantServiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55004c0694ad59f2c2c6e8e05ed3b30624f316db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fd93564e8614dbe132c0d452c0221ec9dfa990")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantServiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantServiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

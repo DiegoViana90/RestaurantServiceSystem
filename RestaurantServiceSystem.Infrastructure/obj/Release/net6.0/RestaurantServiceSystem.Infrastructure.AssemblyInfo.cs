a7862debb86435d3303dc140a1d1f520abe2ac00
Diego Viana <dtviana90@gmail.com>
2024-05-05 04:41:42 -0300

    build: altera referencia de projeto

--- RestaurantServiceSystem.Infrastructure/obj/Release/net6.0/RestaurantServiceSystem.Infrastructure.AssemblyInfo.cs
+++ RestaurantServiceSystem.Infrastructure/obj/Release/net6.0/RestaurantServiceSystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantServiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6766c80422908f16d877091d43e366fb3d2522dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55004c0694ad59f2c2c6e8e05ed3b30624f316db")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantServiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantServiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
